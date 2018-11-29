6473a450ef40d5b576de8112443c35c1bccc04c7
ArjenCole-mbp <jinhesky@hotmail.com>
2018-11-29 22:52:05 +0800

    新建目标表页面交互完善
    1.增加日期选择器
    2.完善数据定义
    3.按钮移到下方

--- client/pages/newItem/newItem.js
+++ client/pages/newItem/newItem.js
@@ -1,38 +1,44 @@
 // client/pages/newItem/newItem.js
 
 let app = getApp();
+var util = require('../../utils/util.js');
 
 Page({
 
   data: {
     isSubmit: false,
     warn: "",
-    phone: "",
-    pwd: "",
-    isPub: false,
-    sex: "男"
+    Text: "",
+    LowLimit: "",
+    DateEnd: util.formatDate(new Date()),
+    Bonds:"",
+    defaultDate: util.formatDate(new Date()),
   },
 
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value);
-    let { phone, pwd, isPub, sex } = e.detail.value;
-    if (!phone || !pwd) {
+    let { Text, LowLimit, Bonds } = e.detail.value;
+    if (!Text || !LowLimit) {
       this.setData({
         warn: "手机号或密码为空！",
         isSubmit: true
       })
       return;
     }
-
     this.setData({
       warn: "",
       isSubmit: true,
-      phone,
-      pwd,
-      isPub,
-      sex
+      Text,
+      LowLimit,
+      Bonds
     })
+  },
 
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      DateEnd: e.detail.value
+    })
   },
 
   formReset: function () {
