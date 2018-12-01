0be177c6ee687368a81f0837ee646d811d38a371
ArjenCole-mbp <jinhesky@hotmail.com>
2018-12-01 10:35:38 +0800

    完善新建目标页面
    1.日期选择器结果显示
    2.文本框输入校验
    3.日期校验
    4.必要时调用数字键盘

--- client/pages/newItem/newItem.js
+++ client/pages/newItem/newItem.js
@@ -18,13 +18,34 @@ Page({
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value);
     let { Text, LowLimit, Bonds } = e.detail.value;
-    if (!Text || !LowLimit) {
+    if (!Text || !LowLimit || !Bonds) {
       this.setData({
-        warn: "手机号或密码为空！",
+        warn: "缺少必须的信息！",
         isSubmit: true
       })
       return;
     }
+    if (!/\d/g.test(LowLimit)) {
+      this.setData({
+        warn: "目标次数必须输入数字！",
+        isSubmit: true
+      })
+      return;
+    } if (!/\d/g.test(Bonds)) {
+      this.setData({
+        warn: "保证金额必须输入数字！",
+        isSubmit: true
+      })
+      return;
+    }
+    if (this.data.DateEnd==this.data.defaultDate) {
+      this.setData({
+        warn: "结束日期不能为今天！",
+        isSubmit: true
+      })
+      return;
+    }
+
     this.setData({
       warn: "",
       isSubmit: true,
