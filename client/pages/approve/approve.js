7d596aecabadd775ae6c15b4dfe4b6abab9b5124
ArjenCole-Desktop <jinhesky@hotmail.com>
2018-12-07 21:31:45 +0800

    采用腾讯云登陆、实现批准功能
    1.登陆改用腾讯云获取userInfo
    2.增加Approvals数组记录批准记录
    3.实现批准按钮功能
    4.开启按钮根据屏幕下沿绑定高度

--- client/pages/approve/approve.js
+++ client/pages/approve/approve.js
@@ -14,9 +14,12 @@ Page({
     friendAvatarUrl:"",
     itemText:"",
     itemId:"",
+
     userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    logged: false,
+
+    item: {},
+    approved: false,
   },
 
   /**
@@ -82,40 +85,103 @@ Page({
 
   },
 
+  bindApproveTap: function () {
+    wx.cloud.init();
+    const db = wx.cloud.database();
+    const bond = db.collection('bond');
+
+    var that=this;
+    var pushStr = util.formatDate(new Date()) + this.data.userInfo.openId;
+    const _ = db.command
+    bond.doc(this.data.itemId).update({
+      data: {
+        Approvals: _.push(pushStr)
+      },
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          approved: true
+        })
+      },
+      fail: function (e) {
+        console.log(e);
+      }
+    })
+  },
+
   bindStartTap: function () {
     wx.redirectTo({
-      url: '/pages/index/index'
+      url: '/pages/index/index?logged=true'
     })
   },
   
-  // 获得用户信息----此处不采用腾讯云的方式获得
+  
   autoGetUserInfo: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
+    if (this.data.logged) return
+
+    util.showBusy('正在登录')
+
+    const session = qcloud.Session.get()
+
+    if (session) {
+      qcloud.loginWithCode({
         success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+          this.setData({ userInfo: res, logged: true })
+          app.globalData.userInfo = this.data.userInfo
+          this.getApprovals();
+          util.showSuccess('登录成功')
+        },
+        fail: err => {
+          console.error(err)
+          util.showModel('登录错误', err.message)
+        }
+      })
+    } else {
+      qcloud.login({
+        success: res => {
+          this.setData({ userInfo: res, logged: true })
+          app.globalData.userInfo = this.data.userInfo
+          this.getApprovals();
+          util.showSuccess('登录成功')
+        },
+        fail: err => {
+          console.error(err)
+          util.showModel('登录错误', err.message)
         }
       })
     }
 
+
   },
+
+  getApprovals:function(){
+    var that = this;
+
+    wx.cloud.init();
+    const db = wx.cloud.database();
+    const db_bond = db.collection('bond');
+
+    db_bond.where({
+      _id: this.data.itemId
+    }).get({
+      success: function (res) {
+        console.log(util.formatDate(new Date()) + that.data.userInfo.openId);
+        var tItem = res.data[0];
+        that.setData({
+          approved: that.contains(tItem.Approvals, util.formatDate(new Date()) + that.data.userInfo.openId)
+        })
+      }
+    })
+  },
+
+  contains: function (arr, obj) {
+    if (typeof (arr) == "undefined") return false;
+    var i = arr.length;
+    while (i--) {
+      if (arr[i] === obj) {
+        return true;
+      }
+    }
+    return false;   
+  }
 })
